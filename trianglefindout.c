3c96e5bdc9ec5e03fead3ed762ef3b4873d2c475
NNKANANI <nileshmkanani@gmail.com>
2025-04-18 16:46:26 +0530

    Add files via upload

---
+++ trianglefindout.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int fa,sa,ta;
+	
+	printf("enter the value of fa: ");
+	scanf("%d",&fa);
+	
+	printf("enter the value of fb: ");
+	scanf("%d",&sa);
+	
+	ta=180-(fa+sa);
+	printf("%d",ta);
+	
+	
+}
