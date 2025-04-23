5ff907a2d839e3ba52ee4862894f55484a992d06
NNKANANI <nileshmkanani@gmail.com>
2025-04-23 16:45:05 +0530

    ssd

---
+++ whileloops.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int s,e;
+	
+	printf("enter s:");
+	scanf("%d",&s);
+	
+	printf("enter e:");
+	scanf("%d",&e);
+	
+	while(s<=e){
+		printf("%d\n",s);
+		s+=4;
+	}
+}
