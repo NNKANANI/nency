7515f8ccb7e182f1fb33b41b14ccac7e4658e73a
NNKANANI <nileshmkanani@gmail.com>
2025-04-30 17:20:43 +0530

    Add files via upload

---
+++ PR 4 Q (6).cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k,l;															
+		for(i=5;i>=0;i++)
+		{
+		for(j=1;j<=5-i;j++)
+		{
+			printf("%d",j);										
+		}
+		for(k=1;k<=2*i;k++)
+		{									
+		printf(" ");							
+	}												
+	
+	
+	for(l=5-i;l>=1;l--)
+	{
+		printf("%d",l);
+		
+		}
+		printf("\n");
+	}
+}
+		
